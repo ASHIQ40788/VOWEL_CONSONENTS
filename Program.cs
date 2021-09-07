db43ac3f6d4a174953e32f6ca472084543885b5f
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-07 21:19:38 +0530

    [ASHIQ] added : VOWEL AND CONSONANTS

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VOWEL_CONSONENTS
+{
+    class Program
+    {
+        //2. Write a program to check whether the input alphabet is a vowel or not.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Alphabet");
+            char ch = Convert.ToChar(Console.ReadLine().ToLower());
+            switch (ch)
+            {
+                case 'a':
+                    Console.WriteLine("It is vowel");
+                    break;
+                case 'i':
+                    Console.WriteLine("It is vowel");
+                    break;
+                case 'o':
+                    Console.WriteLine("It is vowel");
+                    break;
+                case 'u':
+                    Console.WriteLine("It is vowel");
+                    break;
+                case 'e':
+                    Console.WriteLine("It is vowel");
+                    break;
+                default:
+                    Console.WriteLine("It Is consonant");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
